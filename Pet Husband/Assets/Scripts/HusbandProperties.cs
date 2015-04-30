d6243ec95ef3e7b4b4a0ca6ba15b88c5321d61b5
MarciaVB <marcia_vb@hotmail.com>
2015-04-30 22:10:55 +0200

    Initial Unity Project commit
    Very basic selection screen. Buttons don't work yet, but you can click
    through the 2 "husbands" :smile:

---
+++ Pet Husband/Assets/Scripts/HusbandProperties.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HusbandProperties : MonoBehaviour {
+	
+	public string _name;
+	public Sprite _image;
+	public string _description;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Pet Husband/Assets/Scripts/SelectionScreen/SelectedHusbandController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SelectedHusbandController : MonoBehaviour {
+
+	//These are needed to connect to the Image & Text objects in the scene
+	public Image _husbandImage;
+	public Text _husbandDescription;
+
+	public GameObject[] _husbandlist;				//List of all available husbands
+	private int _selectedNumber = 0;				//Number to iterate through husbandList
+	private HusbandProperties _selectedHusband;		//Variable to access properties of the selected husband
+	private int _amountOfHusbands;
+	// Use this for initialization
+	void Start () {
+	
+		_amountOfHusbands = _husbandlist.Length-1;
+		UpdateHusband();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButtonUp (0)) {
+			AdvanceInList();
+			UpdateHusband();
+		}
+	}
+
+	private void AdvanceInList() {
+		if (_selectedNumber < _amountOfHusbands)
+			++_selectedNumber;
+		else
+			_selectedNumber = 0;
+	}
+
+	public void UpdateHusband() {
+		_selectedHusband = _husbandlist [_selectedNumber].GetComponent<HusbandProperties> ();
+		_husbandDescription.text = _selectedHusband._description;
+		_husbandImage.sprite = _selectedHusband._image;
+	}
+
+}
