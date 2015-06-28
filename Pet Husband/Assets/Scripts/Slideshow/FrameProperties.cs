2165890dad635062e5b4cb1207aea6b3eb9f5404
MarciaVB <marcia_vb@hotmail.com>
2015-06-28 13:24:04 +0200

    Cutscenes

---
+++ Pet Husband/Assets/Scripts/Slideshow/FrameProperties.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameProperties : MonoBehaviour {
+
+	public Texture _image;
+	public float _time;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
