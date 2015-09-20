b676a954986e1d8605c40b0454b92fe16fc7093b
MarciaVB <marcia_vb@hotmail.com>
2015-09-20 14:54:10 +0200

    Cleanup + task
    Afbeeldingen die niet meer nodig zijn verwijderd + begin van taak-icon

---
+++ Pet Husband/Assets/Scripts/TaskBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaskBehavior : MonoBehaviour {
+
+	public string _text;
+	public Sprite _icon;
+	public Sprite _bgImage;
+
+	private void Start()
+	{
+		this.enabled = false;
+	}
+
+	public void Create(string text, string iconPath)
+	{
+		Sprite icon = Resources.Load(iconPath) as Sprite;
+		this._text = text;
+		this._icon = icon;
+	}
+
+	public void ShowFirstTime()
+	{
+		this.enabled = true;
+	}
+}
---
+++ Pet Husband/Assets/TestActivator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestActivator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButtonUp (0)) 
+		{
+			Canvas task = GameObject.Find("Task").GetComponent<Canvas>();
+			task.enabled = true;
+		}
+	}
+}
