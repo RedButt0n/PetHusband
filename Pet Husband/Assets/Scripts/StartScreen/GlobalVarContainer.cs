bbfabe90a3c9bd675b742ceccb203ca6de69d6cb
MarciaVB <marcia_vb@hotmail.com>
2015-09-20 17:37:30 +0200

    GlobalVarContainer
    Better Global variables management

---
+++ Pet Husband/Assets/Scripts/StartScreen/GlobalVarContainer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalVarContainer : MonoBehaviour {
+
+	public string _playerName = "";
+
+	// Use this for initialization
+	void Start () {
+	
+		if (this.name == "GlobalVarContainer") DontDestroyOnLoad (this.gameObject);
+	}
+}
