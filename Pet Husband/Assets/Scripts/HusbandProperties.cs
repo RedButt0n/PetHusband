7efcd83cf9e2dfabb13264a188a3d743658ab701
MarciaVB <marcia_vb@hotmail.com>
2015-09-22 23:02:44 +0200

    Infoscreen + btn cleanup
    added info screen in selection screen + put correct btn graphics in
    start screen + selection screen + task
    TO DO: Link extra image in info popup in selection screen

--- Pet Husband/Assets/Scripts/HusbandProperties.cs
+++ Pet Husband/Assets/Scripts/HusbandProperties.cs
@@ -5,7 +5,8 @@ public class HusbandProperties : MonoBehaviour {
 	
 	public string _name;
 	public Sprite _image;
-	public string _description;
+	[TextArea(1,5)] public string _description;
+	[TextArea(3,10)] public string _longDescription;
 
 	// Use this for initialization
 	void Start () {
