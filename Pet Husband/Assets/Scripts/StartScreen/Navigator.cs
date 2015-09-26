5793ea7dccc7c8a26f1d529f65d6cb972b44ad76
MarciaVB <marcia_vb@hotmail.com>
2015-09-26 17:40:48 +0200

    Added Esc to exit game

--- Pet Husband/Assets/Scripts/StartScreen/Navigator.cs
+++ Pet Husband/Assets/Scripts/StartScreen/Navigator.cs
@@ -18,6 +18,8 @@ public class Navigator : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		if (Input.GetKeyUp (KeyCode.Escape))
+			Quit ();
 	}
 
 	public void GoToSelectionScreen()
